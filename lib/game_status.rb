b61df6723bfacc4feb5a2aff62e1cfc2485203d9
Paul H Graham Jr <pjhg14@gmail.com>
2021-01-31 00:46:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,9 +13,9 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    print position_1
-    print position_2
-    print position_3
+    print "|" + position_1 + "|"
+    print "|" + position_2 + "|"
+    print "|" + position_3 + "|"
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
