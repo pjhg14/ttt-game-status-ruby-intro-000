2f4131c158d5e57a119d8ec94276014be1b5f309
Paul H Graham Jr <pjhg14@gmail.com>
2021-01-31 00:55:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,14 +21,12 @@ def won?(board)
 
     #print win_combination
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    if position_1 == ("X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
-    else
-      false
     end
+    
   end
+  
   false
 end
 
@@ -37,7 +35,7 @@ def full?(board)
 end
 
 def draw?(board)
-  print board
+  #print board
   full?(board) && won?(board).nil?
 end
 
