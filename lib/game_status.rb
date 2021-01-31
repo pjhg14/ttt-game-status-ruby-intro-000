4af976a78cc3fed25d715ba0875b03bc8d1419d5
Paul H Graham Jr <pjhg14@gmail.com>
2021-01-31 00:39:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,10 +15,9 @@ def won?(board)
 
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       print board
-      #puts win_combination
       return win_combination
     else
-      print board
+      #print board
       false
     end
   end
